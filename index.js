2bd1552f681314dff5b73be9aec3a1b6769bbe29
Leal-Joaquin <Lealjoaquin24@gmail.com>
2024-01-14 21:11:22 -0300

    Primer commit

---
+++ index.js
@@ -0,0 +1,112 @@
+console.log("Hola mundo")
+
+// Inicio de Sesion
+
+const BBDD = [
+    {
+        usuario: "daniel",
+        contraseña: "1123patito"
+    },
+    {
+        usuario: "pedro123",
+        contraseña: "1pedro123"
+    },
+    {
+        usuario: "1martin",
+        contraseña: "1123456"
+    }
+]
+
+const botonIniciar = document.querySelector("#btn-iniciar");
+const contenedor = document.querySelector("#contenedor");
+const inputs = document.querySelectorAll("input");
+
+
+const isLog = JSON.parse(localStorage.getItem("isLog"))
+if(isLog && isLog.usuario){
+    contenedor.innerHTML = `<h4>Bienvenido de nuevo sr/sra <strong> ${isLog.usuario} </strong> </h4>`
+}
+
+const usuarioALoguear = {
+    usuario: '',
+    password: '',
+}
+
+inputs.forEach((input)=> {
+    input.addEventListener("input", ({target: {name, value}})=>{
+        usuarioALoguear[name] = value
+    })
+});
+
+botonIniciar.addEventListener("click", () => {
+
+    const siExiste = BBDD.find((persona) => {
+        return persona.usuario === usuarioALoguear.usuario && persona.contraseña === usuarioALoguear.password
+    })
+
+    if (siExiste !== undefined) {
+        localStorage.setItem("isLog", JSON.stringify ({usuario: usuarioALoguear.usuario}))
+        contenedor.innerHTML = `<h1>Bienvenido de nuevo sr/sra <strong> ${siExiste.usuario} </strong> </h1>`
+    } else {
+        console.log("quien te conoce")
+    }
+
+})
+
+function cargarProducto(producto){
+
+    class Producto {
+        constructor(marca, cantidad, precio){
+            this.marca = marca;
+            this.cantidad = cantidad;
+            this.precio = precio;
+        }
+    }
+    const PRODUCTO = [
+        new Producto (`Cafetera Italiana`, 5, 24.250),
+        new Producto (`Cafetera Ibrik`, 7 , 20.000),
+        new Producto (`Sifon Japones`, 2 , 47.000),
+        new Producto (`Aeropress`, 6 , 15.000),
+        new Producto (`Prensa Francesa`, 3 , 12.000),
+        new Producto (`Chemex`, 3 , 17.500),
+        new Producto (`Filtros x 20u`, 20 , 5.000),
+        new Producto (`Molino`, 15 , 11.300)
+    ];
+    return PRODUCTO;
+}
+const PRODUCTOS = cargarProducto();
+console.log(PRODUCTOS)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
